23851783b810a0d8f9ee69d9e721978df027d5e4
Pablo Camarotta <camarottapablo@gmail.com>
2022-02-11 16:40:39 -0300

    Agrego notificaciones toast

--- API/Errors/ApiResponse.cs
+++ API/Errors/ApiResponse.cs
@@ -17,10 +17,10 @@ namespace API.Errors
         {
             return statusCode switch
             {
-                400 => "A bad request, you have made",
-                401 => "Authorized, you are not",
-                404 => "Resource found, it was not",
-                500 => "Errors are the path to the dark side.  Errors lead to anger.   Anger leads to hate.  Hate leads to career change.",
+                400 => "Esta petición ha arrojado un error",
+                401 => "No está autorizado",
+                404 => "Recurso no encontrado",
+                500 => "Error interno",
                 _ => null
             };
         }
