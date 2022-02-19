e4200e33ce4fbf53718ba87cfac3e73f63cf04e8
Pablo Camarotta <camarottapablo@gmail.com>
2022-02-19 16:11:56 -0300

    Calcular costo de envio

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Basket, IBasket, IBasketItem, IBasketTotals } from '../shared/models/basket';
 import { BehaviorSubject, map } from 'rxjs';
 import { IProduct } from '../shared/models/product';
+import { IDeliveryMethod } from '../shared/models/deliveryMethod';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +15,14 @@ export class BasketService {
   basket$ = this.basketSource.asObservable();
   private basketTotalSource = new BehaviorSubject<IBasketTotals>(null);
   basketTotal$ = this.basketTotalSource.asObservable();
+  shipping = 0;
 
   constructor(private http: HttpClient) { }
+
+  setShippingPrice(deliveryMethod: IDeliveryMethod) {
+    this.shipping = deliveryMethod.price;
+    this.calculateTotals();
+  }
 
   getBasket(id: string) {
     return this.http.get(this.baseUrl + 'basket?id=' + id)
@@ -90,7 +97,7 @@ export class BasketService {
 
   private calculateTotals() {
     const basket = this.getCurrentBasketValue();
-    const shipping = 0;
+    const shipping = this.shipping;
     const subtotal = basket.items.reduce((a, b) => (b.price * b.quantity) + a, 0);
     const total = subtotal + shipping;
     this.basketTotalSource.next({shipping, total, subtotal});
